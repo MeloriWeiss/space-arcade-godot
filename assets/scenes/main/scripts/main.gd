6d90ae5b71977cd69a162ab262fb853c57f1a726
Evgeniy Nazarov <zentorno.2000@mail.ru>
2026-04-29 17:26:51 +0300

    Add levels

--- assets/scenes/main/scripts/main.gd
+++ assets/scenes/main/scripts/main.gd
@@ -3,15 +3,42 @@ extends Node2D
 @export var rock_scene : PackedScene
 var screensize = Vector2.ZERO
 
+var level = 0
+var score = 0
+var playing = false
+
+
 func _ready() -> void:
 	screensize = get_viewport().get_visible_rect().size
-	
-	for i in 3:
-		spawn_rock(3)
 
 
 func _process(delta: float) -> void:
-	pass
+	if not playing: return
+	
+	if get_tree().get_nodes_in_group("Rocks").size() == 0:
+		new_level()
+
+func new_game():
+	get_tree().call_group("Rocks", "queue_free")
+	level = 0
+	score = 0
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready!")
+	$Player.reset()
+	await $HUD/Timer.timeout
+	playing = true
+
+func new_level():
+	level += 1
+	$Player.fire_rate -= 0.03
+	
+	var lives = $Player.lives
+	if lives < 3:
+		$Player.set_lives(lives + 1, false)
+	
+	$HUD.show_message("Level %s" % level)
+	for i in level:
+		spawn_rock(3)
 
 func spawn_rock(size, pos=null, vel=null):
 	if pos == null:
@@ -22,6 +49,25 @@ func spawn_rock(size, pos=null, vel=null):
 		vel = Vector2.RIGHT.rotated(randf_range(0, TAU)) * randf_range(50, 125)
 	
 	var r = rock_scene.instantiate()
+	
 	r.screensize = screensize
+	r.exploded.connect(self._on_rock_exploded)
 	r.start(pos, vel, size)
+	
 	call_deferred("add_child", r)
+
+func _on_rock_exploded(size, radius, pos, vel):
+	score += 10 * size
+	$HUD.update_score(score)
+	
+	if size <= 1: return
+	
+	for offset in [-1, 1]:
+		var dir = $Player.position.direction_to(pos).orthogonal() * offset
+		var newpos = pos + dir * radius
+		var newvel = dir * vel.length() * 1.1
+		spawn_rock(size - 1, newpos, newvel)
+
+func game_over():
+	playing = false
+	$HUD.game_over()
--- assets/scenes/player/scripts/player.gd
+++ assets/scenes/player/scripts/player.gd
@@ -4,8 +4,20 @@ extends RigidBody2D
 @export var spin_power = 16000
 
 @export var bullet_scene : PackedScene
-@export var fire_rate = 0.25
+@export var fire_rate = 0.5: set = set_fire_rate
 var can_shoot = true
+
+signal lives_changed
+signal dead
+
+var reset_pos = false
+
+var _lives = 0
+var lives:
+	get:
+		return _lives
+	set(value):
+		set_lives(value)
 
 var thrust = Vector2.ZERO
 var rotation_dir = 0
@@ -16,6 +28,8 @@ var state = INIT
 var screensize = Vector2.ZERO
 
 func _ready() -> void:
+	$Sprite2D.hide()
+	
 	screensize = get_viewport_rect().size
 	change_state(ALIVE)
 	$GunCooldown.wait_time = fire_rate
@@ -23,8 +37,8 @@ func _ready() -> void:
 
 func _process(delta):
 	get_input()
-	
-	
+
+
 func get_input():
 	thrust = Vector2.ZERO
 	if state in [DEAD, INIT]: return
@@ -45,17 +59,32 @@ func _integrate_forces(physics_state):
 	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
 	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
 	physics_state.transform = xform
+	
+	if reset_pos:
+		physics_state.transform.origin = screensize / 2
+		reset_pos = false
 
 func change_state(new_state):
 	match new_state:
 		INIT:
 			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.modulate.a = 0.5
 		ALIVE:
 			$CollisionShape2D.set_deferred("disabled", false)
+			collision_layer = 1
+			collision_mask = 1
+			$Sprite2D.modulate.a = 1.0
 		INVULNERABLE:
-			$CollisionShape2D.set_deferred("disabled", true)
+			$CollisionShape2D.set_deferred("disabled", false)
+			collision_layer = 0
+			collision_mask = 0
+			$Sprite2D.modulate.a = 0.5
+			$InvulnerabilityTimer.start()
 		DEAD:
 			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.hide()
+			linear_velocity = Vector2.ZERO
+			dead.emit()
 	state = new_state
 
 func shoot():
@@ -70,3 +99,45 @@ func shoot():
 
 func _on_gun_cooldown_timeout() -> void:
 	can_shoot = true
+
+func set_lives(value, has_invulnerable = true):
+	_lives = value
+	lives_changed.emit(_lives)
+	
+	if _lives <= 0:
+		change_state(DEAD)
+	if has_invulnerable:
+		change_state(INVULNERABLE)
+
+func reset():
+	reset_pos = true
+	$Sprite2D.show()
+	lives = 3
+	change_state(ALIVE)
+
+
+func _on_invulnerability_timer_timeout() -> void:
+	change_state(ALIVE)
+
+
+func _on_body_entered(body):
+	if state == INVULNERABLE: return
+	
+	if body.is_in_group("Rocks"):
+		body.explode()
+		lives -= 1
+		explode()
+
+func explode():
+	$Explosion.show()
+	$Explosion/AnimationPlayer.play("explosion")
+	await $Explosion/AnimationPlayer.animation_finished
+	$Explosion.hide()
+
+func set_fire_rate(value):
+	if value < 0.001:
+		fire_rate = 0.001
+		$GunCooldown.wait_time = fire_rate
+	else:
+		fire_rate = value
+		$GunCooldown.wait_time = fire_rate
