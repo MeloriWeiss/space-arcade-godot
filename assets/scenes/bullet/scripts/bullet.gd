2e6a8e077ceb715d8c122a99edccaa6c990f07a5
Evgeniy Nazarov <zentorno.2000@mail.ru>
2026-04-29 11:57:53 +0300

    Initial commit

---
+++ assets/scenes/bullet/scripts/bullet.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+@export var speed = 1000
+
+var velocity = Vector2.ZERO
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	position += velocity * delta
+
+
+func start(_transform):
+	transform = _transform
+	velocity = transform.x * speed
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Rocks"):
+		body.explode()
+		queue_free()
---
+++ assets/scenes/player/scripts/player.gd
@@ -0,0 +1,72 @@
+extends RigidBody2D
+
+@export var engine_power = 500
+@export var spin_power = 16000
+
+@export var bullet_scene : PackedScene
+@export var fire_rate = 0.25
+var can_shoot = true
+
+var thrust = Vector2.ZERO
+var rotation_dir = 0
+
+enum { INIT, ALIVE, INVULNERABLE, DEAD }
+var state = INIT
+
+var screensize = Vector2.ZERO
+
+func _ready() -> void:
+	screensize = get_viewport_rect().size
+	change_state(ALIVE)
+	$GunCooldown.wait_time = fire_rate
+
+
+func _process(delta):
+	get_input()
+	
+	
+func get_input():
+	thrust = Vector2.ZERO
+	if state in [DEAD, INIT]: return
+	
+	if Input.is_action_pressed("thrust"):
+		thrust = transform.x * engine_power
+	if Input.is_action_pressed("shoot"):
+		shoot()
+	
+	rotation_dir = Input.get_axis("rotate_left", "rotate_right")
+
+func _physics_process(delta):
+	constant_force = thrust
+	constant_torque = rotation_dir * spin_power
+
+func _integrate_forces(physics_state):
+	var xform = physics_state.transform
+	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
+	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
+	physics_state.transform = xform
+
+func change_state(new_state):
+	match new_state:
+		INIT:
+			$CollisionShape2D.set_deferred("disabled", true)
+		ALIVE:
+			$CollisionShape2D.set_deferred("disabled", false)
+		INVULNERABLE:
+			$CollisionShape2D.set_deferred("disabled", true)
+		DEAD:
+			$CollisionShape2D.set_deferred("disabled", true)
+	state = new_state
+
+func shoot():
+	if state == INVULNERABLE or can_shoot == false: return
+	
+	can_shoot = false
+	$GunCooldown.start()
+	var b = bullet_scene.instantiate()
+	get_tree().root.add_child(b)
+	b.start($Muzzle.global_transform)
+
+
+func _on_gun_cooldown_timeout() -> void:
+	can_shoot = true
